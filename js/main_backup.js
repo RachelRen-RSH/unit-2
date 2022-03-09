718eb0ae7952b756fb8982175e7868be32c8f2fc
RachelRen-RSH <71853197+RachelRen-RSH@users.noreply.github.com>
2022-03-09 00:04:17 -0600

    lab1finished

--- js/main_backup.js
+++ js/main_backup.js
@@ -2,21 +2,16 @@
 var body = document.getElementsByTagName("body")[0];
 body.style.backgroundColor = "#c1c9cc";
 
-//create the title and the button for the Olympics website
 var h1 = document.createElement('h1');
 h1.innerHTML = "Winter Olympics Worldwide Athletes 1994-2022";
-h1.id = "title";
+h1.style.backgroundColor = "#c1c9cc";
 var div = document.getElementById("others");
 div.appendChild(h1);
-document.getElementById("title").insertAdjacentHTML('afterbegin', '<a href="https://olympics.com/">' + "<img alt='Olympics' id = 'olympicImg' src='../img/noun-olympic.png'" + "</img></a>"
-
-);
 //declare map var in global scope
 var map;
 var minValue = 1;
 var dataStats = [];
 
-//create the popupcontent constructor
 function PopupContent(properties, attribute) {
     this.properties = properties;
     this.attribute = attribute;
@@ -54,11 +49,12 @@ function getData() {
             calcStats(json);
             createPropSymbols(json, attributes);
             createSequenceControls(attributes);
-            createLegend();
+
+            createLegend(attributes);
+
         });
 };
 
-// calculates the values for the legend
 function calcStats(data) {
     //create empty array to store all data values
     var allValues = [];
@@ -77,10 +73,13 @@ function calcStats(data) {
     var sum = allValues.reduce(function (a, b) { return a + b; });
     dataStats.push(sum / allValues.length);
     dataStats.push(Math.min(...allValues));
+
+    //calculate meanValue
+
+    console.log("This is : " + dataStats[0]);
 }
 
-// create the legend for the map
-function createLegend() {
+function createLegend(attributes) {
     var LegendControl = L.Control.extend({
         options: {
             position: 'bottomright'
@@ -92,7 +91,7 @@ function createLegend() {
 
             container.innerHTML = '<p class="temporalLegend">Number of athletes in <span class="year">1994</span></p>';
 
-            //start attribute legend svg string
+            //Step 1: start attribute legend svg string
             var svg = '<svg id="attribute-legend" width="130px" height="130px">';
 
             //add attribute legend svg to container
@@ -100,26 +99,22 @@ function createLegend() {
             //array of circle names to base loop on
             var circles = ["max", "mean", "min"];
 
-            //loop to add each circle and text to svg string
+            //Step 2: loop to add each circle and text to svg string
+            console.log("dataStats is: " + dataStats);
             for (var i = 0; i < circles.length; i++) {
-                //assign the r and cy attributes    
+                //Step 3: assign the r and cy attributes    
+                console.log(dataStats[0]);
                 var radius = calcPropRadius(dataStats[i]);
                 var cy = 130 - radius;
+                console.log("radius is: " + radius);
                 //circle string            
-                svg += '<circle class="legend-circle" id="' + circles[i] + '" r="' + radius + '"cy="' + cy + '" fill="#6495ED" fill-opacity="0.8" stroke="#000000" cx="65"/>';
+                svg += '<circle class="legend-circle" id="' + circles[i] + '" r="' + radius + '"cy="' + cy + '" fill="#F47821" fill-opacity="0.8" stroke="#000000" cx="65"/>';
 
                 //evenly space out labels            
-                var textY = i * 90 + 20;
+                var textY = i * 20 + 20;
 
-                //text string  
-                if (i == 2) {
-                    svg += '<text id="' + circles[i] + '-text" x="125" y="130">' + Math.round(dataStats[i] * 100) / 100 + " athlete" + '</text>'
-                }
-                else {
-                    svg += '<text id="' + circles[i] + '-text" x="125" y="' + textY + '">' + Math.round((dataStats[i] * 100) / 100) + " athletes" + '</text>'
-                }
-
-
+                //text string            
+                svg += '<text id="' + circles[i] + '-text" x="65" y="' + textY + '">' + Math.round(dataStats[i] * 100) / 100 + "athletes" + '</text>'
             };
 
             //close svg string
@@ -134,7 +129,6 @@ function createLegend() {
     map.addControl(new LegendControl());
 
 };
-
 //calculate the radius of each proportional symbol
 function calcPropRadius(attValue) {
     //constant factor adjusts symbol sizes evenly
@@ -149,14 +143,18 @@ function calcPropRadius(attValue) {
     return radius;
 };
 
-
+/////////////////////////////////////////////
 function pointToLayer(feature, latlng, attributes) {
-    //Assign the current attribute based on the first index of the attributes array
+    //Step 4: Assign the current attribute based on the first index of the attributes array
+
     var attribute = attributes[0];
+    //check
+
+    //var attribute = "1994";
     //create marker options
     var options = {
         radius: 8,
-        fillColor: "#6495ED",
+        fillColor: "#90EE90",
         color: "#000",
         weight: 1,
         opacity: 1,
@@ -164,17 +162,22 @@ function pointToLayer(feature, latlng, attributes) {
     };
     //For each feature, determine its value for the selected attribute
     var attValue = Number(feature.properties[attribute]);
+
     //Give each feature's circle marker a radius based on its attribute value
     options.radius = calcPropRadius(attValue);
+
     //create circle marker layer
     var layer = L.circleMarker(latlng, options);
+    // var popupContent = createPopupContent(feature.properties, attribute);
     var popupContent = new PopupContent(feature.properties, attribute);
+
     layer.bindPopup(popupContent.formatted);
+
     //return the circle marker to the L.geoJson pointToLayer option
     return layer;
 }
+////////////////////////////////////////////
 
-// create the proportional symbols and add to the map
 function createPropSymbols(data, attributes) {
     //create a Leaflet GeoJSON layer and add it to the map
     L.geoJson(data, {
@@ -223,26 +226,26 @@ function createSequenceControls(attributes) {
     document.querySelector('#forward').insertAdjacentHTML("afterend", '<br><h id = "intervals"><h id = "start">1994</h><h id = "intervalA">1998</h><h id = "intervalB">2002</h><h id = "intervalC">2006</h><h id = "intervalD">2010</h><h id = "intervalE">2014</h><h id = "intervalF">2018</h></h><h id = "end">2022</h>');
 };
 
-// update the size of the proportional symbols for each year
 function updatePropSymbols(attribute) {
     map.eachLayer(function (layer) {
         if (layer.feature) {
+
             //access feature properties
             var props = layer.feature.properties;
+
             //update each feature's radius based on new attribute values
             var radius = calcPropRadius(props[attribute]);
             layer.setRadius(radius);
+
+            // var popupContent = createPopupContent(props, attribute);
             var popupContent = new PopupContent(props, attribute);
             //update popup content            
             popup = layer.getPopup();
             popup.setContent(popupContent.formatted).update();
-            var year = attribute;
-            document.querySelector('.year').innerHTML = year;
         };
     });
 };
 
-// get the attributes and the properties from the geojson data
 function processData(data) {
     //empty array to hold attributes
     var attributes = [];
@@ -257,7 +260,22 @@ function processData(data) {
             attributes.push(attribute);
         }
     };
+
+    //check result
+
+
     return attributes;
 };
+
+function createPopupContent(props, attribute) {
+    var popupContent = "<p><b>Country:</b> " + props.Country + "</p>";
+
+    //add formatted attribute to panel content string
+
+    popupContent += "<p><b>" + "Number of athletes in <b>" + attribute + ":</b> " + props[attribute] + "</p>";
+    return popupContent;
+}
+
+
 document.addEventListener('DOMContentLoaded', createMap);
 
